1a2aaf13f519684608f0b948682904a0c8509e99
pranjals149 <42092291+pranjals149@users.noreply.github.com>
2020-05-30 13:32:19 +0530

    Day38

---
+++ routes/leaderRouter.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const Leaders = require('../models/leaders');
+var authenticate = require('../authenticate');
+var cors = require('./cors');
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+    .options(cors.corsWithOptions, (req, res) => { res.sendStatus = 200; })
+    .get(cors.cors, (req, res, next) => {
+        Leaders.find({})
+        .then((leaders) => {
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(leaders);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+})
+    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        Leaders.create(req.body)
+        .then((leader) => {
+            console.log('Leaders Created');
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(leader);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+})
+    .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        res.end("Operation not supported");
+})
+    .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        console.log('Deleting all Leaders');
+        Leaders.remove({})
+        .then((resp) => {
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(resp);
+        }, (err) => next(err))
+        .catch((err) => next(err))
+});
+
+leaderRouter.route('/:leaderId')
+    .options(cors.corsWithOptions, (req, res) => { res.sendStatus = 200; })
+    .get(cors.cors, (req, res, next) => {
+        Leaders.findById(req.params.leaderId)
+        .then((leaders) => {
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(leaders);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+})
+    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        res.end("POST operation not supported on leaderId's: " + req.params.leaderId);
+})
+    .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        Leaders.findByIdAndUpdate(req.params.leaderId, { $set: req.body }, { new: true })
+        .then((leaders) => {
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(leaders);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+})
+    .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        console.log("Deleting all Leaders");
+        Leaders.findByIdAndRemove(req.params.leaderId)
+        .then((resp) => {
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(resp);
+        }, (err) => next(err))
+        .catch((err) => next(err))
+})
+
+module.exports = leaderRouter;
---
+++ routes/promoRouter.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const Promotions = require('../models/promotions');
+var authenticate = require('../authenticate');
+var cors = require('./cors');
+
+const promoRouter = express.Router();
+
+promoRouter.use(bodyParser.json());
+
+promoRouter.route('/')
+    .options(cors.corsWithOptions, (req, res) => { res.sendStatus = 200; })
+    .get(cors.cors, (req, res, next) => {
+        Promotions.find({})
+        .then((promotions) => {
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(promotions);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+})
+    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        Promotions.create(req.body)
+        .then((promotion) => {
+            console.log('Promotions Created');
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(promotion);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+})
+    .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        res.end("Operation not supported");
+})
+    .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        console.log('Deleting all Promotions');
+        Promotions.remove({})
+        .then((resp) => {
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(resp);
+        }, (err) => next(err))
+        .catch((err) => next(err))
+});
+
+promoRouter.route('/:promoId')
+    .options(cors.corsWithOptions, (req, res) => { res.sendStatus = 200; })
+    .get(cors.cors, (req, res, next) => {
+        Promotions.findById(req.params.promoId)
+        .then((promotions) => {
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(promotions);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+})
+    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        res.end("POST operation not supported on promoId's: " + req.params.promoId);
+})
+    .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        Promotions.findByIdAndUpdate(req.params.promoId, { $set: req.body }, { new: true })
+        .then((promotions) => {
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(promotions);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+})
+    .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        console.log("Deleting all Promotions");
+        Promotions.findByIdAndRemove(req.params.promoId)
+        .then((resp) => {
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(resp);
+        }, (err) => next(err))
+        .catch((err) => next(err))
+})
+
+module.exports = promoRouter;
